028e39203a4e2e89df4bab85828989946baf356e
Greg Murray <gremurra@gmail.com>
2020-04-17 10:03:17 -0400

    category service added

---
+++ ElevenNote.Services/CategoryService.cs
@@ -0,0 +1,49 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class CategoryService
+    {
+        public bool CreateCategory(CategoryCreate model)
+        {
+            var entity =
+                new Category()      //creating the instance of Category
+                {
+                    CategoryName = model.CategoryName,
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Categories.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+
+        }
+
+        public IEnumerable<CategoryListItem> GetCategories()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Categories
+                    .Select(
+                        e =>
+                        new CategoryListItem
+                        {
+                            CategoryId = e.CategoryId,
+                            CategoryName = e.CategoryName
+                        }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
--- ElevenNote.WebAPI/Controllers/CategoryController.cs
+++ ElevenNote.WebAPI/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +8,20 @@ using System.Web.Http;
 
 namespace ElevenNote.WebAPI.Controllers
 {
+    [Authorize]
     public class CategoryController : ApiController
     {
+        private CategoryService CreateCategoryService()
+        {
+            var categoryService = new CategoryService();
+            return categoryService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            CategoryService categoryService = CreateCategoryService();
+            var categories = categoryService.GetCategories();
+            return Ok(categories);
+        }
     }
 }
